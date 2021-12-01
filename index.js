e83944b466674891e9f1094c02b5c8352e2130eb
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-12-01 12:09:27 +0300

    role

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+
+const db = require("./db/db.js");
+
+const app = express();
+app.use(express.json());
+
+
+
+const roleRouter = require("./routers/routes/role");
+app.use( roleRouter);
+
+
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
---
+++ routers/controllers/user.js
@@ -0,0 +1,65 @@
+const userModel = require("./../../db/models/user");
+require("dotenv").config();
+
+
+const jwt = require("jsonwebtoken");
+const SECRETKEY = process.env.SECRETKEY;
+
+
+const bcrypt = require("bcrypt");
+const SALT = Number(process.env.SALT);
+
+const signUp = async (req, res) => {
+  const { email, password, role } = req.body;
+  const saveEmail = email.toLowerCase();
+  const savePass = await bcrypt.hash(password, SALT);
+
+  const newUser = new userModel({
+    email: saveEmail,
+    password: savePass,
+    role,
+  });
+
+  newUser
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+const logIn = (req, res) => {
+  const { email, password } = req.body;
+  const saveEmail = email.toLowerCase();
+
+  userModel
+    .findOne({ email: saveEmail })
+    .then(async (result) => {
+      if (result) {
+        if (saveEmail == result.email) {
+         
+          const savePass = await bcrypt.compare(password, result.password); 
+          if (savePass) {
+            const payload = {
+              role: result.role,
+            };
+            const token = await jwt.sign(payload, SECRETKEY);
+            res.status(200).json({ result, token });
+          } else {
+            res.status(400).json("invalid email or password");
+          }
+        } else {
+          res.status(400).json("invalid email or password");
+        }
+      } else {
+        res.status(404).json("not found");
+      }
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+};
+
+module.exports = { signUp, logIn };
