22708654b399ad09666076407aacb16b92086d89
AbdulhakimAloudah <Abdulhakim.Ali.elq.js@tuwaiq.edu.sa>
2021-12-01 18:45:28 +0300

    commintS

--- db/models/comment.js
+++ db/models/comment.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const comment = new mongoose.Schema({
+const commentS = new mongoose.Schema({
   desc: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
@@ -8,4 +8,4 @@ const comment = new mongoose.Schema({
   timeStamp: { type: Date, default: new Date() },
 });
 
-module.exports = mongoose.model("Comment", comment);
+module.exports = mongoose.model("Comment", commentS);
